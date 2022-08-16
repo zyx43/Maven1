1eca1047e8c9efe2f79c47cc016f117e52102078
zyx <zyx@nju.com>
2022-08-16 15:39:34 +0800

    git test first commit master

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,20 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String a="1234";
+        a+="4";
+        System.out.println(a.getClass().getTypeName());
+        String b="1234";
+        System.out.println(b.getClass().getTypeName());
+        System.out.println(a==b);
+        System.out.println("test git version 1");
+
+    }
+}
